cef768e8baf1ef9f660729f06249c21e353110bc
derickson.loss <derickson.loss@paylivre.com>
2021-11-29 12:16:04 -0300

    cria hook personalizado de useCount

--- src/count-context.js
+++ src/count-context.js
@@ -23,4 +23,12 @@ function CountProvider({children}) {
     return <CountContext.Provider value={value}>{children}</CountContext.Provider>
 }
 
+function useCount() {
+    const context = React.useContext(CountContext);
+    if (context === undefined) {
+        throw new Error('useCount must be used within a CountProvider')
+    }
+    return context;
+}
+
 export {CountProvider}
